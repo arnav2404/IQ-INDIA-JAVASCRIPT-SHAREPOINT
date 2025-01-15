eec3061702d29335d5925b1fd1e110aaa0cb2073
arnav2404 <114469735+arnav2404@users.noreply.github.com>
2025-01-15 15:04:58 +0530

    15-JAN

---
+++ 15-JAN-PROJECT-12/15-JAN-PROJECT-12.js
@@ -0,0 +1,42 @@
+document.write(`
+<div>
+    <h1 style="width: 30%; text-align: center; margin: auto;"><hr>🛒Grocery Store<hr></h1>
+    <h2 style="width: 30%; text-align: center; margin: auto;">-------------🧾Reciept------------</h2>
+    <table border="1" style=" margin: auto; border-collapse: collapse; width:30%;" cellpadding="8">
+        <tr>
+            <th>Items</th>
+            <th style="text-align:right;">Quantity</th>
+        </tr>
+        <tr>
+            <td>🥛Milk</td>
+            <td style="text-align:right;">2</td>
+        </tr>
+        <tr>
+            <td>🍞Bread</td>
+            <td style="text-align:right;">1</td>
+        </tr>
+        <tr>
+            <td>🥚Eggs</td>
+            <td style="text-align:right;">2</td>
+        </tr>
+        <tr>
+            <td>🍚Rice</td>
+            <td style="text-align:right;">5</td>
+        </tr>
+        <tr>
+            <td>🧈Butter</td>
+            <td style="text-align:right;">1</td>
+        </tr>
+        <tr>
+            <td>🍎Apples</td>
+            <td style="text-align:right;">6</td>
+        </tr>
+        <tr>
+            <td>🧾Total</td>
+            <td style="text-align:right;">27</td>
+        </tr>
+    </table>
+    <br><hr style="width: 30%; text-align: center; margin: auto;">
+    <p style="width: 30%; text-align: right; margin: auto;"><b>💵Total Cost:27.00</b></p>
+    <p style="text-align: center;">✨Thank you for Shopping with us! 😊</p>
+</div>`)
